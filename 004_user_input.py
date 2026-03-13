3229e74f7270b3644e6f1690cd0021b510d720ef
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-03-13 23:25:35 +0530

    refresh user input

---
+++ 004_user_input.py
@@ -0,0 +1,6 @@
+# input() - a function that prompts the user to enter data; returns the entered data as a string
+
+a = input()
+print(a)
+a = input("Name?: ") # type cast if expected i/p not string
+print(a)
