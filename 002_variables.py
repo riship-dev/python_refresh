62383dc04cf881b754c06f762efe622f280f3f99
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-03-13 22:39:42 +0530

    refresh variables

---
+++ 002_variables.py
@@ -0,0 +1,19 @@
+# variable: reusable container for a value; behaves as if it is the value contained
+
+# strings
+first_name = "Rishi"
+print(first_name)
+print(f"My name is {first_name}") # f-strings
+
+# integers
+age = 23
+print(f"i am {age} years old")
+
+# float
+price = 10.92
+print(price)
+
+# boolean
+is_student = True # 1
+is_teacher = False # 0 None {} []
+print(is_student, is_teacher)
