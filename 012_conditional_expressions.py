a71c99b3a13bc554742c6e7005fe2eabe045ee5c
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-03-15 03:29:15 +0530

    refresh conditional expressions

---
+++ 012_conditional_expressions.py
@@ -0,0 +1,10 @@
+# conditional expression:
+# a one-line shortcut for the if-else statement (ternary operator)
+# print or assign one of two values based on condition
+# X if conditon else Y
+
+num = 50
+print("hi" if num == 5 else "hello")
+a = 2
+b = 3
+max_num = a if a > b else b 
