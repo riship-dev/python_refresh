176ce7d8dcd754e350c06057fdf1b6b445b3448c
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-03-27 01:41:24 +0530

    refresh functions

---
+++ 031_functions.py
@@ -0,0 +1,11 @@
+# function: 
+# a block of reusable code
+# place () after function name to invoke it
+
+def happy_birthday(name, age):
+    print(f"Happy birthday {name}")
+    print(f"You are {age} years old")
+
+happy_birthday("Rishi", 22)
+happy_birthday("John Doe", 34)
+happy_birthday("Jane Smith", 45)
