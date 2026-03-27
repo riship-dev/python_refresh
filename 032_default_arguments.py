ddd1d305e365a71c0fd5046e4113fee6e62647ba
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-03-27 20:35:47 +0530

    refresh default arguments

---
+++ 032_default_arguments.py
@@ -0,0 +1,17 @@
+'''
+default arguments:
+A default value for certain parameters
+default used when the argument is omitted
+makes your functions more flexible, reduces number of arguments
+1 - positional
+2 - default
+3 - keyword
+4 - arbitrary
+'''
+
+def net_price(list_price, discount=0, tax=0.05):
+    return list_price * (1 - discount) * (1 + tax)
+
+print(net_price(500))
+print(net_price(500, 0.1))
+print(net_price(500, 0.1, 0))
