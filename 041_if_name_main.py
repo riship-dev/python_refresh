4ce0a0afd1ca64e8ed686ebf81c23e415878110a
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2026-03-28 04:44:31 +0530

    refresh dunder name

---
+++ 041_if_name_main.py
@@ -0,0 +1,15 @@
+'''
+if __name__ == __main__:
+[this script can be imported or standalone]
+functions and classes in this module can be reused without the main block of code executing
+
+ex. library:
+import library for functionality
+when running library directly, display a help page
+'''
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
